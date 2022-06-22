71fa0a0a26ab8412432705b43d565d366bac9312
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-22 13:06:46 +0100

    Better error handling and display

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout } from "../components";
 import { gql, useQuery } from "@apollo/client";
 import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 export const TRACKS = gql`
   query getTracks {
@@ -23,14 +24,13 @@ export const TRACKS = gql`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
-
   return (
     <Layout grid>
-      {data?.tracksForHome?.map((track) => (
-        <TrackCard key={track.id} track={track} />
-      ))}
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
     </Layout>
   );
 };
