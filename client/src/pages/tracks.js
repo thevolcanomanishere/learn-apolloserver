4d8dbbb83ced4c6d2fc076dde3089ba517380467
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-22 12:45:06 +0100

    Getting plain JSON data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
